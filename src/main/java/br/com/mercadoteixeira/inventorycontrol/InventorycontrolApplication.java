ba49fd8d93db1a72040c4b2ccee5442093617802
Conrado Pereira <conrado.spereira@gmail.com>
2022-06-23 23:07:02 -0300

    Adding the correct webjars.

--- src/main/java/br/com/mercadoteixeira/inventorycontrol/InventorycontrolApplication.java
+++ src/main/java/br/com/mercadoteixeira/inventorycontrol/InventorycontrolApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventorycontrolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventorycontrolApplication.class, args);
+		SpringApplication.run(InventorycontrolApplication.class, args); 
 	}
 
 }
