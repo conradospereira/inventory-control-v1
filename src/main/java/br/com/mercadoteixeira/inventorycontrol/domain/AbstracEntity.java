12ed4a1026348daf66870a390607f10ebd162fbf
conrado <conrado.spereira@gmail.com>
2022-06-26 09:46:27 -0300

    Declaring JPA dependency.

---
+++ src/main/java/br/com/mercadoteixeira/inventorycontrol/domain/AbstracEntity.java
@@ -0,0 +1,47 @@
+package br.com.mercadoteixeira.inventorycontrol.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstracEntity<ID extends Serializable> implements Serializable {
+    
+
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstracEntity<?> other = (AbstracEntity<?>) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
